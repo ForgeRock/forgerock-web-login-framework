2186f8a9b433c6b7ab234f434991749a31901877
Justin Lowery <justin.lowery@forgerock.com>
2022-08-23 16:06:08 -0500

    fix(feedback): address ryan's PR feedback

--- src/lib/utilities/i18n.utilities.ts
+++ src/lib/utilities/i18n.utilities.ts
@@ -57,7 +57,7 @@ export function interpolate(
   if (!key) throw new Error('No key provided to t()');
 
   // Grab the text from the translations store.
-  const contentObj = get(strings) as { [key: string]: string };
+  const contentObj = get<Record<string, string> | null>(strings);
   const string = (contentObj && contentObj[key]) || '';
 
   let messageDirty = '';
