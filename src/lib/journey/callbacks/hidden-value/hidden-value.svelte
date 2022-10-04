090e35c2df50fd7e0aac1a9eb499418ebc28b70d
David Adams <david.adams@forgerock.com>
2022-10-04 13:25:43 +0100

    feat(callbacks): add hidden-value callback and radio option for choice

---
+++ src/lib/journey/callbacks/hidden-value/hidden-value.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import type { HiddenValueCallback } from '@forgerock/javascript-sdk';
+
+  export let callback: HiddenValueCallback;
+  export let firstInvalidInput: boolean;
+  export let idx: number;
+</script>
--- tests/widget/modal/widget-modal.login-with-confirm.test.js
+++ tests/widget/modal/widget-modal.login-with-confirm.test.js
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('Modal widget with simple login and confirmation', async ({ page }) => {
-  await page.goto('widget/modal?journey=LoginWithConfirmation');
+  await page.goto('widget/modal?journey=LoginTestCallbacks');
 
   const loginButton = page.locator('button', { hasText: 'Open Login Modal' });
   await loginButton.click();
@@ -12,7 +12,13 @@ test('Modal widget with simple login and confirmation', async ({ page }) => {
   await page.fill('text=Password', 'j56eKtae*1');
   await page.locator('button', { hasText: 'Next' }).click();
 
-  expect(await page.locator('text="Are you human?"')).toContainText('Are you human?');
+  // Confirmation
+  expect(await page.locator('text="Are you human?"').innerText()).toBe('Are you human?');
+  await page.selectOption('select', '0');
+  await page.locator('button', { hasText: 'Next' }).click();
+
+  // Choice
+  expect(await page.locator('text="Are you sure?"').innerText()).toBe('Are you sure?');
   await page.selectOption('select', '0');
   await page.locator('button', { hasText: 'Next' }).click();
 
