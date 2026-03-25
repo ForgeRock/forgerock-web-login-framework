e578d0adabeeeeb75a5395b47b9c57fc98d94698
Ryan Bas <ryanbas21@gmail.com>
2026-03-25 15:46:32 -0600

    chore: formatting

--- apps/login-app/src/routes/api/tokens/+server.ts
+++ apps/login-app/src/routes/api/tokens/+server.ts
@@ -13,19 +13,17 @@ import type { RequestHandler } from './$types';
 import { AM_DOMAIN_PATH, OAUTH_REALM_PATH } from '$core/constants';
 
 export const POST: RequestHandler = async (event: RequestEvent) => {
-  const bodyStream = event?.request?.body;
-  const body = bodyStream?.getReader().read();
+  const body = await event.request.text();
 
   const response = await fetch(`${AM_DOMAIN_PATH}${OAUTH_REALM_PATH}/access_token`, {
     method: 'POST',
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
     },
-    body: body?.toString(),
+    body,
   });
 
   const resBody = await response.text();
-  // console.log(resBody);
 
   return new Response(resBody);
 };
