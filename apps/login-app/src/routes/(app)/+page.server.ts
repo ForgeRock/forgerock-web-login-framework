20aed5e7aa9f8210b0f6067283983f136678f33d
Vatsal Parikh <vatsalparikh11@outlook.com>
2026-04-14 12:30:47 -0700

    feat(login-app): redirect url (#454)
    * feat(login-app): redirect uri
    
    * feat(login-app):added edge cases and readme for redirect logic
    
    * feat(login-app): redirect with hidden form and actions
    
    * feat(login-app): refactored redirect logic to functional, composable, and declarative, moved functions to appropriate files

--- apps/login-app/src/routes/(app)/+page.server.ts
+++ apps/login-app/src/routes/(app)/+page.server.ts
@@ -1,19 +1,25 @@
 /**
  *
- * Copyright © 2025 Ping Identity Corporation. All right reserved.
+ * Copyright © 2025-2026 Ping Identity Corporation. All right reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  *
  **/
 
-import type { RequestEvent } from '@sveltejs/kit';
+import { redirect, type RequestEvent } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import type { z } from 'zod';
+import { z } from 'zod';
 
 import { getLocale } from '$core/_utilities/i18n.utilities';
+import { resolveRedirect } from '$server/redirect/redirect.utilities';
 
 import type { stringsSchema } from '$core/locale.store';
+import {
+  storeRedirectParams,
+  createRedirectContext,
+  readAndClearRedirectCookie,
+} from '$server/redirect/redirect.effects';
 
 export const load: PageServerLoad = async (event: RequestEvent) => {
   const userLocale = event.request.headers.get('accept-language') || 'en-US';
@@ -33,5 +39,20 @@ export const load: PageServerLoad = async (event: RequestEvent) => {
     localeContent = await import(`$locales/us/en/index.json`);
   }
 
-  return { content: localeContent.default };
+  const redirectParams = storeRedirectParams(event);
+
+  return {
+    content: localeContent.default,
+    redirectParams,
+  };
+};
+
+export const actions = {
+  default: async (event: RequestEvent) => {
+    const formData = await event.request.formData();
+    const cookie = readAndClearRedirectCookie(event);
+    const redirectContext = await createRedirectContext(formData, cookie);
+    const url = resolveRedirect(redirectContext);
+    redirect(303, url);
+  },
 };
