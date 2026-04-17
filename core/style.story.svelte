be6f0123e2e85c5668f1f14985f68480e40bdcfb
Vatsal Parikh <vatsalparikh11@outlook.com>
2026-04-17 08:32:58 -0700

    refactor(journey): update copyright headers

--- core/style.story.svelte
+++ core/style.story.svelte
@@ -1,6 +1,6 @@
 <!--
 
- Copyright © 2025 Ping Identity Corporation. All right reserved.
+ Copyright © 2025-2026 Ping Identity Corporation. All right reserved.
  
  This software may be modified and distributed under the terms
  of the MIT license. See the LICENSE file for details.
